a47dd847899eab2a47ac41090066f884c8ad8c2a
Gokalp <gokalppabdullah@gmail.com>
2026-06-10 00:25:45 +0300

    Repository context

---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext : DbContext 
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
